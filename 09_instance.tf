aa133409c8593539fcfee73cc66040449d8db93f
@sachinkashyap-exe <110095982+sachinkashyap-exe@users.noreply.github.com>
2023-05-21 23:26:56 +0530

    09_instance.tf

--- 09_instance.tf
+++ 09_instance.tf
@@ -5,13 +5,13 @@ resource "aws_instance" "s1" {
      subnet_id                   =  aws_subnet.mySubnet.id
      vpc_security_group_ids      = [aws_security_group.allow_tls.id]
      associate_public_ip_address = true
-     user_data                   = <<USER_DATA
+     user_data                   = <<-EOF
      #!/bin/bash
          sudo yum install httpd -y
          sudo systemctl enable httpd
          sudo systemctl start httpd
       echo '<html><h1>Hello From Your first Web Server!</h1></html>' > /var/www/html/index.html
-         USER_DATA
+         EOF
          tags = {
      Name = "server1"
          source = "terraform"
